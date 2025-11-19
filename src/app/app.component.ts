4d906e9accec159383a1305d379af8c2763e7811
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-11-19 09:16:25 -0300

    páginas iniciais adicionada!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Home', url: '/home', icon: 'mail' },
+    { title: 'Reservas', url: '/reservas', icon: 'paper-plane' },
+    { title: 'Calendario', url: '/calendario', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,21 @@ export const routes: Routes = [
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },  {
+    path: 'reservas',
+    loadComponent: () => import('./reservas/reservas.page').then( m => m.ReservasPage)
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'calendario',
+    loadComponent: () => import('./calendario/calendario.page').then( m => m.CalendarioPage)
+  },
+
 ];
