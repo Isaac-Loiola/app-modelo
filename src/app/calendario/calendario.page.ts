7ee6821aec8c2bae21dcae44079550a59e4b170b
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-11-19 11:38:49 -0300

    Método para adicionar uma array de datas

--- src/app/calendario/calendario.page.ts
+++ src/app/calendario/calendario.page.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonMenuButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonDatetime, IonList, IonLabel, IonListHeader, IonItem } from '@ionic/angular/standalone';
+// import { IonicModule } from '@ionic/angular'
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.page.html',
   styleUrls: ['./calendario.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonDatetime, IonList, IonLabel, IonListHeader, IonItem]
 })
 export class CalendarioPage implements OnInit {
 
   constructor() { }
+
+  dataSelectionada: string = '';
+  listaDatas: Date[] = [];
+
+  adicionarData(){
+    if(this.dataSelectionada){
+      const nova = new Date(this.dataSelectionada);
+      const existe = this.listaDatas.some(
+        d => d.toDateString() === nova.toDateString()
+      );
+      if(!existe)
+        this.listaDatas.push(nova);
+    }
+  }
+  removerData(i: number){
+    this.listaDatas.slice(i, 1);
+  }
 
   ngOnInit() {
   }
